714e6bc3e1e79b529b9045b32a9505bf65902aa5
Nam Vo <vhnam2504@gmail.com>
2019-05-03 14:23:12 +0700

    chore(setup): define structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Registration from './scenes/Registration/Registration';
+
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/scenes/Registration/components/SuccessForm/SuccessForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SuccessForm = () => {
+  return <div>SuccessForm</div>;
+};
+
+export default SuccessForm;
