3f73f21b861ea1a4045bacdcc81e218bfb94b5ca
Nam Vo <vhnam2504@gmail.com>
2019-05-03 16:24:33 +0700

    feat(Registration): finish step 3 - ConfirmForm

--- src/scenes/Registration/components/ConfirmForm/ConfirmForm.js
+++ src/scenes/Registration/components/ConfirmForm/ConfirmForm.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const ConfirmForm = () => {
-  return <div>ConfirmForm</div>;
+const ConfirmForm = ({fieldValues, previousStep, confirm}) => {
+  return (
+    <div>
+      <h2>Confirm Registration</h2>
+      <ul>
+        <li>
+          <strong>Name:</strong> {fieldValues.name}
+        </li>
+        <li>
+          <strong>Email:</strong> {fieldValues.email}
+        </li>
+        <li>
+          <strong>Age:</strong> {fieldValues.age}
+        </li>
+        <li>
+          <strong>Intruments:</strong> {fieldValues.instruments.join(', ')}
+        </li>
+      </ul>
+      <ul className="form-fields">
+        <li className="form-footer">
+          <strongutton
+            className="btn btn-default pull-left"
+            onClick={previousStep}
+          >
+            Back
+          </strongutton>
+          <strongutton className="btn btn-primary pull-right" onClick={confirm}>
+            Submit Registration
+          </strongutton>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ConfirmForm;
