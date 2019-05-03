664c9d491c5077d88c31c8bd9abcdb0974e833c3
Nam Vo <vhnam2504@gmail.com>
2019-05-03 16:25:36 +0700

    feat(Registration): finish step 4 - SuccessForm

--- src/scenes/Registration/components/SuccessForm/SuccessForm.js
+++ src/scenes/Registration/components/SuccessForm/SuccessForm.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const SuccessForm = () => {
-  return <div>SuccessForm</div>;
+  return (
+    <div>
+      <h2>Successfully Registered!</h2>
+      <p>
+        Please check your email <strong>{fieldValues.email}</strong> for a
+        confirmation link to activate your account.
+      </p>
+    </div>
+  );
 };
 
 export default SuccessForm;
